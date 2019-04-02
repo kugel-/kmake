kugel-/kmake

commit 426df92f564905e8f8857b82fd6911e5e12aa1d2

add libtooltag attribute
When set (to CC), then libtool will use gcc for linking C++ binaries.
This can avoid pulling libstdc++ which may be desirable for some projects.

The default hasn't changed, which is to use CXX for C++ sources and CC
for C sources automatically.

kmake.mk
@@ -173,6 +173,9 @@ is_lib = $(filter %.la %.a,$(1))
 # returns CXX if one or more C++ files are found, else CC
 getcc = $(or $($(call varname,$(2))-compiler),$(if $(call is_cxx,$(1)),$(CXX),$(CC)))
 # call with $(1) = target (incl. extension)
+# returns the LTTAG flag, or CXX if one or more C++ files are found, else CC
+getlttag = $(or $($(call varname,$(1))-libtooltag),$(if $(call is_cxx,$(call getsrc,$(1))),CXX),CC)
+# call with $(1) = target (incl. extension)
 getdepsdir = $(dir $(1)).deps/
 # call with $(1) = target (incl. extension)
 # Note this is returns empty if the target has no source files, since it is
@@ -232,7 +235,7 @@ $(OUTDIR)$(1): KM_CFLAGS   := $(KM_CFLAGS)   $(KM_CFLAGS_$(if $(call is_lib,$(3)
 $(OUTDIR)$(1): KM_CXXFLAGS := $(KM_CXXFLAGS) $(KM_CXXFLAGS_$(if $(call is_lib,$(3)),LIB,PROG)) $(call getvar,$(3),CXXFLAGS)
 $(OUTDIR)$(1): COMPILE_FLAGS = $(if $(call is_cxx,$(2)),$$(KM_CXXFLAGS) $$(CXXFLAGS),$$(KM_CFLAGS) $(CFLAGS))
 $(OUTDIR)$(1): PRINTCMD = $(if $(call is_cxx,$(2)),CXX,CC)
-$(OUTDIR)$(1): LTTAG = $(if $(call is_cxx,$(2)),CXX,CC)
+$(OUTDIR)$(1): LTTAG = $(call getlttag,$(2))
 $(OUTDIR)$(1): COMPILE = $(call getcc,$(2),$(3))
 $(OUTDIR)$(1): CMD = $$(COMPILE) $$(KM_CPPFLAGS) $$(CPPFLAGS) $$(COMPILE_FLAGS)
 $(OUTDIR)$(1): PATTERN = %$(suffix $(2))
@@ -259,7 +262,7 @@ cleanfiles += $(if $(call getobj,$(1)),$(OUTDIR)$(call getoldcmdfile,$(1)))
 
 $(OUTDIR)$(1): KM_LDFLAGS := $(KM_LDFLAGS) $(KM_LDFLAGS_$(if $(call is_lib,$(1)),LIB,PROG)) $(call getvar,$(1),LDFLAGS)
 $(OUTDIR)$(1): PRINTCMD = $(if $(call is_cxx,$(call getsrc,$(1))),CXXLD,CCLD)
-$(OUTDIR)$(1): LTTAG = $(if $(call is_cxx,$(call getsrc,$(1))),CXX,CC)
+$(OUTDIR)$(1): LTTAG = $(call getlttag,$(1))
 $(OUTDIR)$(1): LINK = $(call getcc,$(call getsrc,$(1)),$(1))
 $(OUTDIR)$(1): CMD = $$(COMPILE) $$(RPATH) $$(KM_LDFLAGS) $$(LDFLAGS) -- $(call getvar,$(1),LIBS)
 $(OUTDIR)$(1): PATTERN = $(addprefix %,$(sort $(suffix $(call getobj,$(1)))))

README
@@ -212,6 +212,12 @@ Pre-defined properties include:
     The compiler program to use to build the targets, for example
     to use clang on a per-var basis.
 
+`-libtooltag`
+    When libtool is used to build shared libraries, this property overrides
+    the tag that libtool uses. By default kmake passes CXX or CC depending
+    on the source files. Use this to enforce, for example, CC.
+    See https://www.gnu.org/software/libtool/manual/html_node/Tags.html.
+
 `-dir`
     The installation directory used at `make install` time.
 

commit 6aeabb71cc1c690e8131cd5b504630338e8571a5

The libtooltag attribute was derived from the wrong var
In this context, $(2) is the source file and $(3) the target that has
attribute originally, so we want the latter.

Fixes: 426df92f5 add libtooltag attribute

kmake.mk
@@ -252,7 +252,7 @@ $(OUTDIR)$(1): KM_CFLAGS   := $(KM_CFLAGS)   $(KM_CFLAGS_$(if $(call is_lib,$(3)
 $(OUTDIR)$(1): KM_CXXFLAGS := $(KM_CXXFLAGS) $(KM_CXXFLAGS_$(if $(call is_lib,$(3)),LIB,PROG)) $(call getvar,$(3),CXXFLAGS)
 $(OUTDIR)$(1): COMPILE_FLAGS = $(if $(call is_cxx,$(2)),$$(KM_CXXFLAGS) $$(CXXFLAGS),$$(KM_CFLAGS) $(CFLAGS))
 $(OUTDIR)$(1): PRINTCMD = $(if $(call is_cxx,$(2)),CXX,CC)
-$(OUTDIR)$(1): LTTAG = $(call getlttag,$(2))
+$(OUTDIR)$(1): LTTAG = $(call getlttag,$(3))
 $(OUTDIR)$(1): COMPILE = $(call getcc,$(2),$(3))
 $(OUTDIR)$(1): CMD = $$(COMPILE) $$(KM_CPPFLAGS) $$(CPPFLAGS) $$(COMPILE_FLAGS)
 $(OUTDIR)$(1): PATTERN = %$(suffix $(2))